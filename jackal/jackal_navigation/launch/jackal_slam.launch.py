4fa98abde502e5f961e14273bafe3bf95ec9bc8f
NiltonSV <incripcion18@gmail.com>
2025-09-22 12:10:09 -0300

    add slam with namespace

--- jackal/jackal_navigation/launch/jackal_slam.launch.py
+++ jackal/jackal_navigation/launch/jackal_slam.launch.py
@@ -2,109 +2,95 @@ from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch.actions import (
     DeclareLaunchArgument,
-    EmitEvent,
-    LogInfo,
-    RegisterEventHandler,
-    OpaqueFunction
+    GroupAction,
+    IncludeLaunchDescription,
 )
 from launch.conditions import IfCondition
-from launch.events import matches_action
+from launch.conditions import UnlessCondition
+from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import (
-    AndSubstitution,
     LaunchConfiguration,
-    NotSubstitution,
-    PathJoinSubstitution
+    PathJoinSubstitution,
+    PythonExpression
 )
-from launch_ros.actions import LifecycleNode
-from launch_ros.events.lifecycle import ChangeState
-from launch_ros.event_handlers import OnStateTransition
-from lifecycle_msgs.msg import Transition
+from launch_ros.actions import (
+    Node,
+    PushROSNamespace,
+    SetRemap
+)
 from nav2_common.launch import ReplaceString
-import yaml
-
-
-def setup_slam(context, *args, **kwargs):
-    """Setup and launch slam_toolbox node."""
-    autostart = LaunchConfiguration('autostart')
-    namespace = LaunchConfiguration('namespace').perform(context)
-    prefix = LaunchConfiguration('prefix').perform(context)
-    slam_params_file = LaunchConfiguration('slam_params_file').perform(context)
-    use_lifecycle_manager = LaunchConfiguration("use_lifecycle_manager")
-    use_sim_time = LaunchConfiguration('use_sim_time')
-
-    # Dynamically set frames names based on the prefix and namespace launch arguments.
-    slam_params = {}
-    namespace_prefix = prefix
-    with open(slam_params_file, 'r') as file:
-        yaml_content = yaml.safe_load(file)
-        slam_params = yaml_content['slam_toolbox']['ros__parameters']
-        if namespace:
-            namespace_prefix = f'{namespace}/{prefix}'
-        slam_params['odom_frame'] = f'{namespace_prefix}{slam_params['odom_frame']}'
-        slam_params['base_frame'] = f'{namespace_prefix}{slam_params['base_frame']}'
-        # map_frame must be the same (map) for each robot
-
-    additional_params = {
-        'use_lifecycle_manager': use_lifecycle_manager,
-        'use_sim_time': use_sim_time
-    }
-
-    # based on https://github.com/SteveMacenski/slam_toolbox/blob/ros2/launch/online_async_launch.py
-    start_async_slam_toolbox_node = LifecycleNode(
-        parameters=[additional_params, slam_params],
-        package='slam_toolbox',
-        executable='async_slam_toolbox_node',
-        name='slam_toolbox',
-        output='screen',
-        namespace=namespace,
-        remappings=[('/map', 'map'),
-                    ('/map_metadata', 'map_metadata'),
-                    ('/map_updates', 'map_updates')]
-    )
-
-    configure_event = EmitEvent(
-        event=ChangeState(
-            lifecycle_node_matcher=matches_action(start_async_slam_toolbox_node),
-            transition_id=Transition.TRANSITION_CONFIGURE
-        ),
-        condition=IfCondition(AndSubstitution(autostart, NotSubstitution(use_lifecycle_manager)))
-    )
-
-    activate_event = RegisterEventHandler(
-        OnStateTransition(
-            target_lifecycle_node=start_async_slam_toolbox_node,
-            start_state="configuring",
-            goal_state="inactive",
-            entities=[
-                LogInfo(msg="[LifecycleLaunch] Slamtoolbox node is activating."),
-                EmitEvent(event=ChangeState(
-                    lifecycle_node_matcher=matches_action(start_async_slam_toolbox_node),
-                    transition_id=Transition.TRANSITION_ACTIVATE
-                ))
-            ]
-        ),
-        condition=IfCondition(AndSubstitution(autostart, NotSubstitution(use_lifecycle_manager)))
-    )
-
-    return [start_async_slam_toolbox_node, configure_event, activate_event]
 
 
 def generate_launch_description():
+    current_pkg = get_package_share_directory('jackal_navigation')
+    namespace = LaunchConfiguration('namespace')
+    prefix = LaunchConfiguration('prefix')
+    slam_launch_path = PathJoinSubstitution(
+        [get_package_share_directory('slam_toolbox'), 'launch', 'online_async_launch.py']
+    )
+    slam_params_file = LaunchConfiguration('slam_params_file')
+    use_sim_time = LaunchConfiguration('use_sim_time')
 
-    slam_params_file = PathJoinSubstitution(
-        [get_package_share_directory("jackal_navigation"), 'config', 'slam_parameters.yaml'])
-
-    params_file = ReplaceString(
+    # --- Handle placeholders inside slam_params_file ---
+    # Placeholders (<robot_namespace>, <robot_namespace_prefix>) are replaced
+    # so that SLAM can run with multiple robots in unique namespaces.
+    slam_params_file = ReplaceString(
         source_file=slam_params_file,
-        replacements={'SPARSE_NORMAL_CHOLESKY': (LaunchConfiguration('namespace'), '/')},
+        replacements={
+            '<robot_namespace_prefix>': [namespace, '/', prefix],
+            '<robot_namespace>': [namespace, '/']
+        },
+        condition=UnlessCondition(
+            PythonExpression([
+                '"',
+                namespace,
+                '" == ""',
+            ])  # check if namespace is empty
+        )
+    )
+    slam_params_file = ReplaceString(
+        source_file=slam_params_file,
+        replacements={
+            '<robot_namespace_prefix>': [prefix],
+            '<robot_namespace>': ['']
+        },
+        condition=IfCondition(
+            PythonExpression([
+                '"',
+                namespace,
+                '" == ""',
+            ])  # check if namespace is empty
+        )
+    )
+
+    start_slam_toolbox_cmd = GroupAction(
+        actions=[
+            PushROSNamespace(namespace),  # Push all nodes under namespace
+            SetRemap(src='/scan', dst='scan'),
+            SetRemap(src='/map', dst='map'),
+            IncludeLaunchDescription(
+                PythonLaunchDescriptionSource(slam_launch_path),
+                launch_arguments={
+                    'slam_params_file': slam_params_file,
+                    'use_sim_time': use_sim_time,
+                }.items()
+            )
+        ]
+    )
+
+    rviz2_path = PathJoinSubstitution([current_pkg, 'rviz', 'jackal.rviz'])
+
+    rviz2_node = Node(
+        condition=IfCondition(LaunchConfiguration('rviz')),
+        package='rviz2',
+        executable='rviz2',
+        output='screen',
+        namespace=namespace,
+        arguments=['-d', rviz2_path],
+        parameters=[{'use_sim_time': use_sim_time}],
     )
 
     args = [
-        DeclareLaunchArgument(
-            'autostart',
-            default_value='true',
-            description='Automatically startup the slamtoolbox. Ignored when use_lifecycle_manager is true.'
-        ),
         DeclareLaunchArgument(
             'namespace',
             default_value='',
@@ -116,14 +102,17 @@ def generate_launch_description():
             description='Prefix added to the jackal link and joint names'
         ),
         DeclareLaunchArgument(
-            'slam_params_file',
-            default_value=slam_params_file,
-            description='Full path to the ROS2 parameters file to use for the slam_toolbox node'
+            'rviz',
+            default_value='false',
+            choices=['true', 'false'],
+            description='Whether to execute rviz2'
         ),
         DeclareLaunchArgument(
-            'use_lifecycle_manager',
-            default_value='false',
-            description='Enable bond connection during node activation'
+            'slam_params_file',
+            default_value=PathJoinSubstitution(
+                [current_pkg, 'config', 'slam.yaml']
+            ),
+            description='Full path to the slam_toolbox config file'
         ),
         DeclareLaunchArgument(
             'use_sim_time',
@@ -133,5 +122,8 @@ def generate_launch_description():
         ),
     ]
 
-    ld = LaunchDescription([*args, OpaqueFunction(function=setup_slam, args=[params_file])])
-    return ld
+    return LaunchDescription([
+        *args,
+        start_slam_toolbox_cmd,
+        rviz2_node,
+    ])
